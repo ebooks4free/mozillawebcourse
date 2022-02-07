005afa373c1434f013ce3f57903cbcd9a7fcee3c
ebooks4free <99144642+ebooks4free@users.noreply.github.com>
2022-02-07 16:26:31 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
+    if(mySrc === 'https://imgur.com/k5bRmkt.png') {
       myImage.setAttribute('src','https://imgur.com/rtyBVJr.png');
     } else {
-      myImage.setAttribute('src','images/firefox-icon.png');
+      myImage.setAttribute('src','https://imgur.com/k5bRmkt.png');
     }
 }
 
